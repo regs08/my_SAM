e29864591b044a6f2774cf526e9af9914033590d
cregs08 <cole.regnier@gmail.com>
2023-04-17 17:14:09 -0300

    fixing typo

--- ConvertToJson/convert.py
+++ ConvertToJson/convert.py
@@ -4,7 +4,7 @@ from my_SAM.config import CAT_ID_MAP
 import os
 
 
-def convert_sam_predictions_to_coco(image_path, masks, cat_id=0, image_id=0):
+def convert_sam_predictions_to_coco(image_path, masks, cat_ids: list, image_id=0):
     """
     here we take in an image path and the masks generated on it b SAM.
     it is assumed that we have one category.
@@ -16,7 +16,7 @@ def convert_sam_predictions_to_coco(image_path, masks, cat_id=0, image_id=0):
     filename = os.path.basename(image_path)
 
     img = create_image_annotation(filename, w,h, image_id=image_id)
-    anns = create_prediction_submask_anns(masks, cat_id=cat_id, image_id=image_id)
+    anns = create_prediction_submask_anns(masks, cat_ids=cat_ids, image_id=image_id)
 
     coco_format['images'] = img
     coco_format['annotations'] = anns
