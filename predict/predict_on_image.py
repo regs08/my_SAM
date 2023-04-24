344cf79f35f9f48eaaaa92f540a3689996f29d04
cregs08 <cole.regnier@gmail.com>
2023-04-24 17:50:13 -0300

    changing the output to a dict containing masks, cat_ids, filenames for each image

--- predict/predict_on_image.py
+++ predict/predict_on_image.py
@@ -5,6 +5,7 @@ from yolo_data.Conversions.convert_yolo_pascal_voc import convert_yolo_to_pascal
 import numpy as np
 import cv2
 import torch
+import os
 
 
 def apply_sam_to_image_folder_with_boxes(img_dir, ann_dir, predictor):
@@ -13,38 +14,37 @@ def apply_sam_to_image_folder_with_boxes(img_dir, ann_dir, predictor):
     :param img_dir: image folder
     :param ann_dir: ann folder
     :param predictor: instance of SAM predictor
-    :return: a list of masks [m, N,C,H,W]; where m is the mask on the image, N is the number of instances, C (?) (h,w) of image
-            a list of class_ids that will have the same len as N in the corresponding mask
+    :return: a list of dictionaries containing the masks, class ids and filename for a given image
     """
     image_paths = glob_image_files(img_dir)
-    masks = []
-    all_class_ids = []
+    output = []
+
     for img_path in image_paths:
-      mask, class_ids = apply_sam_to_image_with_bboxes(img_path, ann_dir, predictor)
-      masks.append(mask)
-      all_class_ids.append(class_ids)
-    return masks, all_class_ids
+        data = apply_sam_to_image_with_bboxes(img_path, ann_dir, predictor)
+        output.append(data)
+
+    return output
 
 
 def apply_sam_to_image_with_bboxes(img_path, ann_dir, sam_predictor):
     """
     TODO experiment with the point labels (background, foreground)
     takes in a img path loads it, finds its corresponding annotation, loads in the bbox from the annotation, then uses
-    SAM to perform instance segmentation. returns a tensor mask in the form of (N, C, H,W)
+    SAM to perform instance segmentation. returns a data package with masks, class ids and filename
     :param img_path: path to load image
     :param ann_dir: folder where anns are stored. note filename w/o ext must==image filename
     :param sam_predictor: predictor instance of SAM
-    :return: torch tensor of mask
+    :return: data package with masks, class ids and filename
     """
     #get ann path
     ann_path = get_annotation_path(img_path, ann_dir)
 
     #loading in bbox and class_ids - assumed that boxes are in yolo -
-    bboxes, class_ids = get_yolo_bboxes_from_txt_file(ann_path)
+    bboxes, cat_ids = get_yolo_bboxes_from_txt_file(ann_path)
     print(f'num boxes(instances) in file: {len(bboxes)}')
 
     #loading in labels
-    labels = [cat_id_map[id] for id in class_ids]
+    labels = [cat_id_map[id] for id in cat_ids]
     print('unique labels: ', np.unique(np.array(labels)))
 
     #loading in image
@@ -69,6 +69,11 @@ def apply_sam_to_image_with_bboxes(img_path, ann_dir, sam_predictor):
       boxes=transformed_bboxes_from_file,
       multimask_output=False,
     )
+    data = {
+        'masks': masks,
+        'cat_ids': cat_ids,
+        'filename': os.path.basename(img_path)
+    }
 
-    return masks, class_ids
+    return data
 
