2b914b7a43f5e3a1aa0c9fe7312e7dbcae386d3f
cregs08 <cole.regnier@gmail.com>
2023-05-02 15:08:27 -0300

    updating maps

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 #default
-ID_TO_LABEL_MAP = {0: 'grape', 1: 'grape 0', 2: 'grape 1'}
+#setting first value to 1. default for coco dataset is 0
+ID_TO_LABEL_MAP = {1: 'grape', 2: 'grape 0', 3: 'grape 1'}
 LABEL_TO_ID_MAP = {v: k for k, v in ID_TO_LABEL_MAP.items()}
 
 
