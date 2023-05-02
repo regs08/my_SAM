20c8c5ff4cc7b5dc17e0853b6bd7298f158b25b5
cregs08 <cole.regnier@gmail.com>
2023-05-02 14:45:10 -0300

    first upload

---
+++ Visualize/utils.py
@@ -0,0 +1,78 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def convert_yolo_to_pascal_voc(yolo_box, image_height, image_width, normilized=True):
+
+  center_x, center_y, width, height = yolo_box
+  xmin = int((center_x - width/2) * image_width)
+  ymin = int((center_y - height/2) * image_height)
+  xmax = int((center_x + width/2) * image_width)
+  ymax = int((center_y + height/2) * image_height)
+  box = [xmin, ymin, xmax, ymax]
+  return box
+
+
+def plot_image_with_yolo_annotations(image_path, annotation_path):
+    """
+    Load an image and its annotations (bounding box and segmentation) in YOLO format,
+    and plot the image with the annotations overlaid on top.
+
+    Args:
+        image_path (str): The path to the input image file.
+        annotation_path (str): The path to the annotation file in YOLO format.
+
+    Returns:
+        None.
+    """
+    # Load the image
+    image = cv2.imread(image_path)
+    h, w, _ = image.shape
+    # Create a blank image with the same dimensions as the original image
+    masked_image = np.zeros((h, w, 3), dtype=np.uint8)
+
+
+
+    # Load the annotations
+    with open(annotation_path, 'r') as f:
+        for line in f:
+            h, w, _ = image.shape
+            h = int(h)
+            w = int(w)
+            annotation = line.strip().split(' ')
+            class_label = int(annotation[0])
+            bbox_norm = np.array([float(x) for x in annotation[1:5]])
+            segmentation_norm = np.array([float(x) for x in annotation[5:]])
+
+            # Convert normalized bbox to pixel coordinates
+            x_min, y_min, x_max, y_max = convert_yolo_to_pascal_voc(bbox_norm, image_height=h, image_width=w)
+
+            # Convert normalized segmentation points to pixel coordinates
+            segmentation = segmentation_norm.reshape(-1, 2) * np.array([w, h])
+            segmentation = segmentation.astype(np.int32)
+
+            # Create a binary mask from the segmentation points
+            mask = np.zeros((h, w), dtype=np.uint8)
+            cv2.fillPoly(mask, [segmentation], 255)
+
+            # Set the color and transparency of the mask
+            mask_color = (255, 0, 0)
+            mask_alpha = 0.5
+
+            # Apply the color and transparency to the mask
+            mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+            mask = mask.astype(np.float32) / 255.0
+            mask = mask * mask_color
+            mask = (mask_alpha * mask).astype(np.uint8)
+
+            # Apply the mask to the blank image
+            masked_image = cv2.addWeighted(masked_image, 1, mask, mask_alpha, 0)
+
+            # Draw the bounding box on the image
+            cv2.rectangle(masked_image, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+
+    masked_image = cv2.addWeighted(masked_image, 1, image, mask_alpha, 0)
+    # Display the image
+    plt.imshow(cv2.cvtColor(masked_image, cv2.COLOR_BGR2RGB))
+    plt.show()
