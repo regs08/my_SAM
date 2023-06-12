fd3c231c38f77e897ffe1b52a3c264ea596bd730
cregs08 <cole.regnier@gmail.com>
2023-06-12 19:58:51 -0300

    adding in image exts

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 #setting first value to 1. default for coco dataset is 0
 ID_TO_LABEL_MAP = {1: 'grape', 2: 'grape 0', 3: 'grape 1'}
 LABEL_TO_ID_MAP = {v: k for k, v in ID_TO_LABEL_MAP.items()}
+image_exts = ['.jpg', '.png', '.jpeg']
 
 
 
